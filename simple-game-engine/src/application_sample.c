4de4312c4524cd7b78104df8dc44b73def16c8a5
Gabriel Trabasso <gabr.j.t@gmail.com>
2021-09-15 00:10:35 -0300

    Define macros to register components and create entities

--- simple-game-engine/src/application_sample.c
+++ simple-game-engine/src/application_sample.c
@@ -10,16 +10,37 @@
 #include "system_screen_clear_background.h"
 #include "system_set_screen_text_world_time.h"
 
+#define REGISTER_COMPONENTS(ecs_world) \
+    ECS_COMPONENT(ecs_world, ComponentTranslation2); \
+    ECS_COMPONENT(ecs_world, ComponentScreenClearBackground); \
+    ECS_COMPONENT(ecs_world, ComponentScreenText); \
+    ECS_TAG(ecs_world, TagSetWorldTime); \
+    ECS_TAG(ecs_world, TagDrawFps);
+
+#define CREATE_ENTITY_CLEAR_BACKGROUND(ecs_world, entity, ...) \
+    const ecs_entity_t entity = ecs_new_id(ecs_world); \
+    ecs_set(ecs_world, entity, ComponentScreenClearBackground, { __VA_ARGS__ });
+
+#define CREATE_ENTITY_TEXT(ecs_world, entity, x, y, text, font_size, ...) \
+    const ecs_entity_t entity = ecs_new_id(ecs_world); \
+    ecs_set(ecs_world, entity, ComponentTranslation2, { x, y }); \
+    ecs_set(ecs_world, entity, ComponentScreenText, { text, font_size, __VA_ARGS__ });
+
+#define CREATE_ENTITY_WORLD_TIME(ecs_world, entity, x, y, text, font_size, ...) \
+    CREATE_ENTITY_TEXT(ecs_world, entity, x, y, text, font_size, __VA_ARGS__) \
+    ecs_add(ecs_world, entity, TagSetWorldTime);
+
+#define CREATE_ENTITY_FPS(ecs_world, entity, x, y) \
+    const ecs_entity_t entity = ecs_new_id(ecs_world); \
+    ecs_set(ecs_world, entity, ComponentTranslation2, { x, y }); \
+    ecs_add(ecs_world, entity, TagDrawFps);
+
 void RegisterSampleApplication()
 {
     ecs_world_t* ecs_world = GetApplicationEcsWorld();
 
     // Register Components
-    ECS_COMPONENT(ecs_world, ComponentTranslation2);
-    ECS_COMPONENT(ecs_world, ComponentScreenClearBackground);
-    ECS_COMPONENT(ecs_world, ComponentScreenText);
-    ECS_TAG(ecs_world, TagSetWorldTime);
-    ECS_TAG(ecs_world, TagDrawFps);
+    REGISTER_COMPONENTS(ecs_world);
 
     // Register Simulation Systems
     RegisterSystemSetScreenTextWorldTime(ecs_world);
@@ -32,23 +53,9 @@ void RegisterSampleApplication()
     RegisterSystemEndDrawing(ecs_world);
 
     // Create Entities
-    const ecs_entity_t entity_screen_clear_background = ecs_new_id(ecs_world);
-    ecs_set(ecs_world, entity_screen_clear_background, ComponentScreenClearBackground, { DARKGRAY });
-
-    const ecs_entity_t entity_congrats_text = ecs_new_id(ecs_world);
-    ecs_set(ecs_world, entity_congrats_text, ComponentTranslation2, { GetScreenWidth() * 0.5f - 750, GetScreenHeight() * 0.5f });
-    ecs_set(ecs_world, entity_congrats_text, ComponentScreenText, { "Congrats! You created your first window! Powered by raylib", 50, LIGHTGRAY });
-
-    const ecs_entity_t entity_ticking_text = ecs_new_id(ecs_world);
-    ecs_set(ecs_world, entity_ticking_text, ComponentTranslation2, { GetScreenWidth() * 0.5f - 200, GetScreenHeight() * 0.5f + 80 });
-    ecs_set(ecs_world, entity_ticking_text, ComponentScreenText, { "flecs is ticking...", 20, LIGHTGRAY });
-
-    const ecs_entity_t entity_display_world_time = ecs_new_id(ecs_world);
-    ecs_set(ecs_world, entity_display_world_time, ComponentTranslation2, { GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f + 80 });
-    ecs_set(ecs_world, entity_display_world_time, ComponentScreenText, { "", 20, RED });
-    ecs_add(ecs_world, entity_display_world_time, TagSetWorldTime);
-
-    const ecs_entity_t entity_display_fps = ecs_new_id(ecs_world);
-    ecs_set(ecs_world, entity_display_fps, ComponentTranslation2, { 10, 10 });
-    ecs_add(ecs_world, entity_display_fps, TagDrawFps);
+    CREATE_ENTITY_CLEAR_BACKGROUND(ecs_world, entity_screen_clear_background, DARKGRAY);
+    CREATE_ENTITY_TEXT(ecs_world, entity_congrats_text, GetScreenWidth() * 0.5f - 750, GetScreenHeight() * 0.5f, "Congrats! You created your first window! Powered by raylib", 50, LIGHTGRAY);
+    CREATE_ENTITY_TEXT(ecs_world, entity_ticking_text, GetScreenWidth() * 0.5f - 200, GetScreenHeight() * 0.5f + 80, "flecs is ticking...", 20, LIGHTGRAY);
+    CREATE_ENTITY_WORLD_TIME(ecs_world, entity_world_time, GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f + 80, "", 20, RED);
+    CREATE_ENTITY_FPS(ecs_world, entity_fps, 10, 10);
 }
